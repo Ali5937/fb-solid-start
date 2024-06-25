a8d44a1e867c265e4ec005cc4a5892df4cbeefa6
Ali5937 <116338481+Ali5937@users.noreply.github.com>
2024-06-25 08:11:04 +0200

    feat: map

---
+++ frontend/src/components/map/drawStyles.tsx
@@ -0,0 +1,121 @@
+import Cookies from "js-cookie";
+import { createEffect } from "solid-js";
+
+const getDrawStyle = (theme: string) => [
+  // ACTIVE (being drawn)
+  // line stroke
+  {
+    id: "gl-draw-line",
+    type: "line",
+    filter: ["all", ["==", "$type", "LineString"], ["==", "active", "true"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "line-dasharray": [0.2, 2],
+      "line-width": 2,
+    },
+  },
+  // polygon fill
+  {
+    id: "gl-draw-polygon-fill",
+    type: "fill",
+    filter: ["all", ["==", "$type", "Polygon"], ["==", "active", "true"]],
+    paint: {
+      "fill-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "fill-outline-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "fill-opacity": 0.1,
+    },
+  },
+  // polygon mid points
+  {
+    id: "gl-draw-polygon-midpoint",
+    type: "circle",
+    filter: ["all", ["==", "$type", "Point"], ["==", "meta", "midpoint"]],
+    paint: {
+      "circle-radius": 3,
+      "circle-color": "#fbb03b",
+    },
+  },
+  // polygon outline stroke
+  // This doesn't style the first edge of the polygon, which uses the line stroke styling instead
+  {
+    id: "gl-draw-polygon-stroke-active",
+    type: "line",
+    filter: ["all", ["==", "$type", "Polygon"], ["==", "active", "true"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "line-dasharray": [0.2, 2],
+      "line-width": 2,
+    },
+  },
+  // vertex point halos
+  {
+    id: "gl-draw-polygon-and-line-vertex-halo-active",
+    type: "circle",
+    filter: ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"]],
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#FFF",
+    },
+  },
+  // vertex points
+  {
+    id: "gl-draw-polygon-and-line-vertex-active",
+    type: "circle",
+    filter: ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"]],
+    paint: {
+      "circle-radius": 3,
+      "circle-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+    },
+  },
+
+  // INACTIVE
+  // line stroke
+  {
+    id: "gl-draw-line-inactive",
+    type: "line",
+    filter: ["all", ["==", "$type", "LineString"], ["==", "active", "false"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "line-width": 3,
+    },
+  },
+  // polygon fill
+  {
+    id: "gl-draw-polygon-fill-inactive",
+    type: "fill",
+    filter: ["all", ["==", "$type", "Polygon"], ["==", "active", "false"]],
+    paint: {
+      "fill-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "fill-outline-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "fill-opacity": 0.1,
+    },
+  },
+  // polygon outline
+  {
+    id: "gl-draw-polygon-stroke-inactive",
+    type: "line",
+    filter: ["all", ["==", "$type", "Polygon"], ["==", "active", "false"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": theme === "dark-theme" ? "#c7cad1" : "#636469",
+      "line-width": 7,
+    },
+  },
+];
+
+export { getDrawStyle };
---
+++ frontend/src/components/map/map.tsx
@@ -0,0 +1,584 @@
+import {
+  createEffect,
+  createSignal,
+  onCleanup,
+  onMount,
+  untrack,
+} from "solid-js";
+import maplibregl from "maplibre-gl";
+import IconPencil from "~/assets/icon-pencil";
+import IconTrash from "~/assets/icon-trash";
+import "./mapLibre.css";
+import "./map.css";
+
+let mapContainer: HTMLDivElement;
+let map: maplibregl.Map;
+export default function Map(props: any) {
+  let draw: any; // type: MapboxGlDraw
+  let drawParent: any;
+  let pencilButton: any;
+  let newPencilButton: any;
+  let trashButton: any;
+  let priceRange: string[] = [];
+  let oldCoordinates: [number, number] = [0, 0];
+  let coordinates: [number, number] = [0, 0];
+  let poly = "";
+  let poly2 = "";
+  let isPopupShown = false;
+  let lowestPrice = 0;
+  let highestPrice = 0;
+  let mapMarkerColor = ["#FF3333", "#2624B3"];
+  let minMaxLng = [0, 0];
+  const [isMapLoaded, setIsMapLoaded] = createSignal(false);
+  const [markers, setMarkers] = createSignal<any>();
+  const [canDraw, setCanDraw] = createSignal(false);
+  const [isDrawing, setIsDrawing] = createSignal(false);
+
+  let markerImg = document.createElement("div");
+  markerImg.setAttribute("class", "map-marker-symbol");
+  let marker = new maplibregl.Marker({
+    element: markerImg,
+  }).setLngLat({
+    lng: 0,
+    lat: 0,
+  });
+
+  function removeOldPolys() {
+    const newMode =
+      draw.getAll().features[draw.getAll().features.length - 1]?.id;
+    draw.getAll().features.forEach((feat: any) => {
+      if (feat.id !== newMode) {
+        draw.delete(feat.id);
+        removeMarkers();
+      }
+    });
+    removeMarkers();
+  }
+
+  async function clickPencil() {
+    setIsDrawing(true);
+    // console.log(draw);
+    if (!draw) {
+      await loadDraw();
+    }
+    draw.changeMode("draw_polygon");
+    // console.log(draw);
+  }
+
+  function clickTrash() {
+    draw?.deleteAll();
+    removeMarkers();
+    handleSearch();
+  }
+
+  function checkIfSelectedItemIsVisibleOnMap() {
+    if (!props.selectedItem()) return;
+    // console.time("test");
+    let isVisible = false;
+    props.propertyItems().forEach((pI: any) => {
+      if (props.selectedItem().id === pI.id) isVisible = true;
+    });
+    if (!isVisible) props.setSelectedItem(null);
+    // console.timeEnd("test");
+  }
+
+  async function handleSearch() {
+    // console.time("fetchTime");
+    let polygonArray: number[][];
+    if (draw && draw.getAll().features[0]) {
+      polygonArray =
+        //@ts-ignore
+        draw?.getAll()?.features[0]?.geometry.coordinates[0];
+      //@ts-ignore
+      const polygon = (await import("turf-polygon")).default;
+      const intersect = (await import("@turf/intersect")).default;
+      //@ts-ignore
+      const featureCollection = (await import("turf-featurecollection"))
+        .default;
+      const selectedShape = polygon([polygonArray]);
+      const mapBounds = polygon([getMapBounds(true)]);
+
+      console.log(selectedShape);
+      console.log(mapBounds);
+      const intersection = intersect(
+        featureCollection([selectedShape, mapBounds])
+      );
+      if (intersection) {
+        const overlappingShape = intersection.geometry.coordinates;
+        poly = turnArrayOfBoundsIntoString(overlappingShape[0]);
+      } else {
+        poly = turnArrayOfBoundsIntoString(
+          map.getZoom() >= 6 ? getMapBounds(true) : getMapBounds(false)
+        );
+      }
+      poly2 = "";
+    } else {
+      //Set polygonArray to the visible screen space
+      polygonArray =
+        map.getZoom() >= 6 ? getMapBounds(true) : getMapBounds(false);
+
+      minMaxLng = [0, 0];
+
+      for (let i = 0; i < polygonArray.length; i++) {
+        if (i === 0) minMaxLng = [polygonArray[i][0], polygonArray[i][0]];
+        if (minMaxLng[0] > polygonArray[i][0])
+          minMaxLng[0] = polygonArray[i][0];
+        if (minMaxLng[1] < polygonArray[i][0])
+          minMaxLng[1] = polygonArray[i][0];
+      }
+
+      const polyWidth = Math.abs(minMaxLng[0] - minMaxLng[1]);
+
+      if (polyWidth > 180) {
+        if (polyWidth > 220) {
+          // console.log(polygonArray);
+          polygonArray = [];
+          poly = "";
+          poly2 = "";
+        } else {
+          splitPolygon(getMapBounds(true));
+        }
+      } else {
+        poly2 = "";
+        poly = turnArrayOfBoundsIntoString(polygonArray);
+      }
+    }
+
+    setPriceRange();
+
+    let type = [0];
+    if (props.itemType() === "apartment") type = [1];
+    else if (props.itemType() === "house") type = [2];
+    else if (props.itemType() === "shared" || props.itemType() === "land")
+      type = [3];
+
+    if (props.saleType() === "buy") type[0] += 3;
+
+    if (minMaxLng[0] >= 62)
+      if (minMaxLng[1] < 180) type[0] += 12;
+      else type = [type[0], type[0] + 6, type[0] + 12];
+    else if (minMaxLng[0] >= -32)
+      if (minMaxLng[1] < 62) type[0] += 6;
+      else if (minMaxLng[1] < 180) type = [type[0] + 6, type[0] + 12];
+      else type = [type[0], type[0] + 6, type[0] + 12];
+    else if (minMaxLng[0] >= -180)
+      if (minMaxLng[1] < 62) type = [type[0], type[0] + 6];
+      else if (minMaxLng[1] < 180) type = [type[0], type[0] + 6, type[0] + 12];
+      else type = [type[0], type[0] + 6, type[0] + 12];
+
+    try {
+      const response = await fetch(
+        `${props.baseUrl}/items?` +
+          new URLSearchParams({
+            type: type.toString(),
+            min: priceRange[0],
+            max: priceRange[1],
+            polygon: poly || "",
+            polygon2: poly2,
+            itemSort: props.itemSort(),
+          })
+      );
+
+      const responseData = await response.json();
+      if (responseData) {
+        lowestPrice = responseData[0].euro_price;
+        highestPrice = responseData[0].euro_price;
+
+        responseData.forEach((el: any) => {
+          const euroPrice = el.euro_price;
+          if (euroPrice < lowestPrice) lowestPrice = euroPrice;
+          if (euroPrice > highestPrice) highestPrice = euroPrice;
+        });
+
+        const reverseData = [...responseData].reverse(); //Todo: check if can be reversed on server
+        const sourceObject = {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: reverseData.map((marker: any) => ({
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [marker.lng, marker.lat],
+              },
+              properties: {
+                lng: marker.lng,
+                lat: marker.lat,
+                size: marker.size,
+                originalPrice: marker.original_price,
+                euroPrice: marker.euro_price,
+                currencyCode: marker.currency_code,
+                currencySymbol: marker.currency_symbol,
+                first_picture: marker.first_picture,
+                id: marker.id,
+                createdAt: marker.created_at,
+              },
+            })),
+          },
+        };
+
+        setMarkers(sourceObject);
+        props.setPropertyItems(responseData);
+
+        checkIfSelectedItemIsVisibleOnMap();
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Fetch request error:", error);
+    }
+
+    // console.timeEnd("fetchTime");
+  }
+
+  function getMapBounds(get4Bounds: boolean) {
+    const bounds = map.getBounds();
+    if (get4Bounds) {
+      return [
+        [bounds._ne.lng, bounds._ne.lat],
+        [bounds._sw.lng, bounds._ne.lat],
+        [bounds._sw.lng, bounds._sw.lat],
+        [bounds._ne.lng, bounds._sw.lat],
+        [bounds._ne.lng, bounds._ne.lat],
+      ];
+    } else {
+      const midPointTop = [
+        (bounds._ne.lng + bounds._sw.lng) / 2,
+        bounds._ne.lat,
+      ];
+      const midPointBottom = [
+        (bounds._sw.lng + bounds._ne.lng) / 2,
+        bounds._sw.lat,
+      ];
+      return [
+        [bounds._ne.lng, bounds._ne.lat],
+        midPointTop,
+        [bounds._sw.lng, bounds._ne.lat],
+        [bounds._sw.lng, bounds._sw.lat],
+        midPointBottom,
+        [bounds._ne.lng, bounds._sw.lat],
+        [bounds._ne.lng, bounds._ne.lat],
+      ];
+    }
+  }
+
+  function splitPolygon(polygon: number[][]) {
+    let midX = (polygon[0][0] + polygon[1][0]) / 2;
+    let polygon1 = [
+      [polygon[0][0], polygon[0][1]],
+      [midX, polygon[1][1]],
+      [midX, polygon[2][1]],
+      [polygon[3][0], polygon[3][1]],
+      [polygon[4][0], polygon[4][1]],
+    ];
+    let polygon2 = [
+      [midX, polygon[0][1]],
+      [polygon[1][0], polygon[1][1]],
+      [polygon[2][0], polygon[2][1]],
+      [midX, polygon[3][1]],
+      [midX, polygon[4][1]],
+    ];
+    poly = turnArrayOfBoundsIntoString(polygon1);
+    poly2 = turnArrayOfBoundsIntoString(polygon2);
+  }
+
+  function turnArrayOfBoundsIntoString(arr: any) {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+      str += `${arr[i][0]}_${arr[i][1]}${i < arr.length - 1 ? "," : ""}`;
+    }
+    return str;
+  }
+
+  function loadMarkers() {
+    const markerCount = markers().data.features.length;
+    removeMarkers();
+    map.addSource("marker-data", {
+      type: "geojson",
+      data: markers().data,
+    });
+    map.addLayer({
+      id: "markers",
+      type: "circle",
+      source: "marker-data",
+      paint: {
+        "circle-radius": 10,
+        "circle-color":
+          markerCount > 1
+            ? [
+                "interpolate",
+                ["linear"],
+                ["get", "euroPrice"],
+                lowestPrice,
+                mapMarkerColor[0],
+                highestPrice,
+                mapMarkerColor[1],
+              ]
+            : mapMarkerColor[0],
+        "circle-stroke-width": 1,
+        "circle-stroke-color":
+          props.theme() === "dark-theme" ? "black" : "white",
+      },
+    });
+  }
+
+  function removeMarkers() {
+    if (map.getLayer("markers")) map.removeLayer("markers");
+    if (map.getSource("marker-data")) map.removeSource("marker-data");
+  }
+
+  function setPriceRange() {
+    priceRange =
+      props.saleType() === "rent"
+        ? props.rentPriceRange()
+        : props.buyPriceRange();
+  }
+
+  // onMount(() => {
+  //   const dummyPencilButton = document.createElement("button");
+  //   dummyPencilButton.id = "dummy-pencil-parent";
+  //   const img = document.createElement("img");
+  //   img.src = pencilIcon;
+  //   img.alt;
+  //   dummyPencilButton.appendChild(img);
+  //   if (!canDraw() && drawParent) drawParent.appendChild(dummyPencilButton);
+
+  //   dummyPencilButton.addEventListener("click", () => {
+  //     setCanDraw(true);
+  //     dummyPencilButton.remove();
+  //     loadDraw();
+  //   });
+  // });
+
+  async function loadDraw() {
+    const MapboxDraw = (await import("@mapbox/mapbox-gl-draw")).default;
+    const FreehandMode = (
+      await import("../../local-modules/local-mapbox-gl-draw-freehand-mode/src")
+    ).default;
+    const { getDrawStyle } = await import("./drawStyles");
+
+    marker.addTo(map);
+
+    map.dragRotate.disable();
+    map.touchZoomRotate.disable();
+    map.keyboard.disableRotation();
+
+    draw = new MapboxDraw({
+      //@ts-ignore
+      modes: Object.assign(MapboxDraw.modes, {
+        draw_polygon: FreehandMode,
+      }),
+      displayControlsDefault: false,
+      styles: getDrawStyle(props.theme()),
+    });
+
+    map.addControl(draw);
+    // pencilButton = document.querySelector(
+    //   ".mapbox-gl-draw_ctrl-draw-btn.mapbox-gl-draw_polygon"
+    // );
+    // const img = document.createElement("img");
+    // img.src = pencilIcon;
+    // pencilButton.appendChild(img);
+    // pencilButton.click();
+  }
+
+  createEffect(() => {
+    const mapLocation = untrack(() => props.mapLocation());
+    map = new maplibregl.Map({
+      container: mapContainer,
+      style: `https://tiles.stadiamaps.com/styles/${
+        props.theme() === "dark-theme" ? "alidade_smooth_dark" : "osm_bright"
+      }.json`,
+      center: [mapLocation[0], mapLocation[1]],
+      zoom: mapLocation[2],
+      minZoom: 1,
+      maxZoom: 20,
+      //   attributionControl: true,
+    });
+
+    let popup = new maplibregl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    function showPopUp(e: any) {
+      const euroPrice = e.features[0].properties.euroPrice;
+      const originalPrice = Number(e.features[0].properties.originalPrice);
+      const currencySymbol = props.currentCurrency()
+        ? props.currentCurrency()[3]
+        : e.features[0].properties.currencySymbol;
+      const size =
+        e.features[0].properties.size *
+        (props.displayUnits() === "f" ? 10.76 : 1);
+      const sizeSymbol =
+        props.displayUnits() === "f" ? `ft<sup>2</sup>` : `m<sup>2</sup>`;
+      const cityName = e.features[0].properties.city;
+      coordinates = e.features[0].geometry.coordinates.slice();
+      if (oldCoordinates[0] === coordinates[0] && isPopupShown) return;
+      isPopupShown = true;
+      oldCoordinates = coordinates;
+      const firstPicture = e.features[0].properties.first_picture;
+      popup
+        .setLngLat(coordinates)
+        .setHTML(
+          `<div class="popup">
+						<img class="popup-img" src="${firstPicture}" />
+						<div class="popup-text">
+							<div class="top">
+								<div class="price">${`${
+                  props.currentCurrency()
+                    ? Math.round(
+                        euroPrice * props.currentCurrency()[4]
+                      ).toLocaleString()
+                    : originalPrice.toLocaleString()
+                } ${currencySymbol}`}</div>
+								<div class="size">${`${Math.round(size)} ${sizeSymbol}`}</div>
+							</div>
+							<div class="city">${cityName ?? ""}</div>
+						</div>
+					</div>`
+        )
+        .addTo(map);
+    }
+
+    map.on("moveend", function () {
+      const center = map.getCenter();
+      const zoom = map.getZoom();
+      props.setMapLocation([
+        center.lng.toFixed(4),
+        center.lat.toFixed(4),
+        zoom.toFixed(1),
+      ]);
+      handleSearch();
+    });
+
+    map.on("draw.modechange", function () {
+      if (draw.getMode() === "draw_polygon") {
+        removeOldPolys();
+        map.getContainer().classList.add("pencil-cursor");
+      } else {
+        map.getContainer().classList.remove("pencil-cursor");
+      }
+    });
+
+    map.on("draw.create", async function () {
+      handleSearch();
+    });
+
+    map.on("mouseenter", "markers", function (e: any) {
+      showPopUp(e);
+    });
+
+    map.on("mousemove", "markers", function (e: any) {
+      showPopUp(e);
+    });
+
+    map.on("mouseleave", "markers", function () {
+      popup.remove();
+      isPopupShown = false;
+    });
+
+    map.on("click", "markers", function (e: any) {
+      showPopUp(e);
+      props.setSelectedItem(e.features[0].properties);
+      props.setIsListOpen(true);
+    });
+
+    map.on("styledata", function () {
+      setIsMapLoaded(true);
+    });
+
+    map.on("load", function () {
+      if (canDraw()) {
+        draw.changeMode("draw_polygon");
+        removeOldPolys();
+        map.getContainer().classList.add("pencil-cursor");
+      }
+    });
+
+    onCleanup(() => {
+      if (pencilButton) pencilButton.remove();
+
+      if (map) {
+        map.remove();
+        marker.remove();
+      }
+    });
+  });
+
+  createEffect(() => {
+    props.theme();
+    props.saleType();
+    props.itemType();
+    props.rentPriceRange();
+    props.buyPriceRange();
+    setPriceRange();
+    removeMarkers();
+    handleSearch();
+    setIsMapLoaded(false);
+
+    // if (props.contrast() === "normal-contrast")
+    //   mapMarkerColor = ["#FF3333", "#2624B3"];
+    // else if (props.theme() === "dark-theme")
+    //   mapMarkerColor = ["#FFE8FE", "#3D86A8"];
+    // else mapMarkerColor = ["#FF3333", "#000080"];
+  });
+
+  createEffect(() => {
+    if (isMapLoaded() && markers()) loadMarkers();
+  });
+
+  //   createEffect(() => {
+  //     if (pencilButton)
+  //       if (props.isListOpen()) pencilButton.classList.add("removed-under-1000");
+  //       else pencilButton.classList.remove("removed-under-1000");
+  //   });
+
+  createEffect(() => {
+    marker.remove();
+    if (props.highlightedItemLngLat()) {
+      marker.setLngLat({
+        lng: props.highlightedItemLngLat()[0],
+        lat: props.highlightedItemLngLat()[1],
+      });
+      marker.addTo(map);
+    } else {
+      if (props.selectedItem()) {
+        marker.setLngLat({
+          lng: props.selectedItem().lng,
+          lat: props.selectedItem().lat,
+        });
+        marker.addTo(map);
+      }
+    }
+  });
+
+  onMount(() => marker.remove());
+
+  return (
+    <div class={`map-parent ${props.isListOpen() ? "list-is-open" : ""}`}>
+      <div ref={drawParent} class="draw-parent">
+        <button
+          ref={newPencilButton}
+          class="pencil-button"
+          aria-label="Pencil"
+          onClick={clickPencil}
+        >
+          <IconPencil />
+        </button>
+        <button
+          ref={trashButton}
+          class="trash-button"
+          aria-label="Trash"
+          onClick={clickTrash}
+        >
+          <IconTrash />
+        </button>
+      </div>
+      <div
+        ref={mapContainer}
+        class={`map maplibregl-map ${isDrawing() ? "pencil-cursor" : ""}`}
+      ></div>
+    </div>
+  );
+}
